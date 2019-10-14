e3446c4c90ab3c0a6a563424c3e67b673307e239
winasya <winasya@mail.ru>
2019-10-15 01:34:25 +0300

    fixed throw&try

--- scanner/ReverseSort.java
+++ scanner/ReverseSort.java
@@ -1,62 +1,60 @@
-import java.util.Arrays;
 import java.io.IOException;
+import java.util.Arrays;
 import java.io.PrintWriter;
 import java.util.Comparator;
 
 public class ReverseSort {
-    public static void main(String[] args) throws IOException{
-        Scanner linesScanner = new Scanner(System.in);
+    public static void main(String[] args) {
+
         int[][] ints = new int[1][];
         long[] sums = new long[1];
         int index = 0;
-        while (linesScanner.hasNextLine()){
-            int[] row = new int[1];
-            String line = linesScanner.nextLine();
-            Scanner intsScanner = new Scanner(line);
-            int currentIndex = 0;
-            long currentSum = 0;
-            while (intsScanner.hasNextInt()) {
-                int current = intsScanner.nextInt();
-                if (currentIndex >= row.length){
-                    row = Arrays.copyOf(row, row.length * 2);
+        try (Scanner linesScanner = new Scanner(System.in)) {
+            while (linesScanner.hasNextLine()) {
+                int[] row = new int[1];
+                String line = linesScanner.nextLine();
+                Scanner intsScanner = new Scanner(line);
+                int currentIndex = 0;
+                long currentSum = 0;
+                while (intsScanner.hasNextInt()) {
+                    int current = intsScanner.nextInt();
+                    if (currentIndex >= row.length) {
+                        row = Arrays.copyOf(row, row.length * 2);
+                    }
+                    currentSum += current;
+                    row[currentIndex++] = current;
                 }
-                currentSum += current;
-                row[currentIndex++] = current;
+                if (index >= ints.length) {
+                    ints = Arrays.copyOf(ints, ints.length * 2);
+                    sums = Arrays.copyOf(sums, ints.length);
+                }
+                ints[index] = Arrays.copyOf(row, currentIndex);
+                sums[index] = currentSum;
+                Arrays.sort(ints[index]);
+                index++;
             }
-            if (index >= ints.length) {
-                ints = Arrays.copyOf(ints, ints.length * 2);
-                sums = Arrays.copyOf(sums, ints.length);
-            }
-            ints[index] = Arrays.copyOf(row, currentIndex);
-            sums[index] = currentSum;
-            Arrays.sort(ints[index]);
-            index++;
+            ints = Arrays.copyOf(ints, index);
+        } catch (IOException e) {
+            System.out.println("Error reading input" + e.getMessage());
+            return;
         }
-        ints = Arrays.copyOf(ints, index);
+
         final long[] fsums = sums;
-        
         Integer[] indexs = new Integer[index];
-        for (int i = 0; i < index; i++){
+        for (int i = 0; i < index; i++) {
             indexs[i] = i;
         }
-        Arrays.sort(indexs, new Comparator<Integer>(){
+        Arrays.sort(indexs, new Comparator<Integer>() {
             @Override
-            public int compare(Integer a, Integer b){
-                long s = fsums[a] - fsums[b];
-                if (s < 0){
-                    return -1;
-                }
-                if (s == 0) {
-                    return 0;
-                }
-                return 1;
+            public int compare(Integer a, Integer b) {
+                return Long.compare(fsums[a], fsums[b]);
             }
         });
-        
+
         PrintWriter output = new PrintWriter(System.out);
-        for (int sortedIndex = index - 1; sortedIndex >= 0; sortedIndex--){
+        for (int sortedIndex = index - 1; sortedIndex >= 0; sortedIndex--) {
             int i = indexs[sortedIndex];
-            for (int j = ints[i].length - 1; j >=0; j--){
+            for (int j = ints[i].length - 1; j >= 0; j--) {
                 output.print(ints[i][j] + " ");
             }
             output.println();
